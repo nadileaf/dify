08efcfc14a8f7e3bba8dab17befca0f2653eaa53
harry <691742@gmail.com>
2024-06-13 16:39:58 +0800

    fix

--- web/app/components/base/logo/logo-site.tsx
+++ web/app/components/base/logo/logo-site.tsx
@@ -11,7 +11,7 @@ const LogoSite: FC<LogoSiteProps> = ({
   return (
     <img
       src='/logo/logo-site.png'
-      className={classNames('block w-auto h-8', className)}
+      className={classNames('block w-auto h-8 opacity-0', className)}
       alt='logo'
     />
   )
